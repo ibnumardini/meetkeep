bd5d6b31610b72d7c9c30db5c490479ab067d8d3
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2026-07-19 22:05:14 +0700

    chore: remove legacy extension build scripts and source files

--- scripts/watch-docs.js
+++ scripts/watch-docs.js
@@ -4,14 +4,6 @@ const path = require('path');
 
 const root = path.join(__dirname, '..');
 
-function build() {
-  try {
-    execSync('bun scripts/build.js', { cwd: root, stdio: 'inherit' });
-  } catch (e) {
-    console.error('Build failed');
-  }
-}
-
 function buildDocs() {
   try {
     execSync('bun scripts/build-docs.js', { cwd: root, stdio: 'inherit' });
@@ -20,17 +12,11 @@ function buildDocs() {
   }
 }
 
-build();
 buildDocs();
-
-chokidar.watch(['src/extension', 'assets'], { cwd: root, ignoreInitial: true }).on('all', (event, file) => {
-  console.log(`${event}: ${file}`);
-  build();
-});
 
 chokidar.watch('src/docs', { cwd: root, ignoreInitial: true }).on('all', (event, file) => {
   console.log(`${event}: ${file}`);
   buildDocs();
 });
 
-console.log('Watching src/extension/, src/docs/, and assets/ ...');
+console.log('Watching src/docs ...');
